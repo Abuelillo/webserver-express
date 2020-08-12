2cf8243270a7c5af894f9c6d2e31c02788da2875
Juan <juanvarios32@gmail.com>
2020-08-12 01:26:32 -0700

    Primera subida

---
+++ README.md
@@ -0,0 +1,10 @@
+## Codigo fuente del proyecto 
+
+Contenido 
+-Master con HBS
+-Separacion de codigo con HBS
+-Servicio con EXPRESS
+
+```
+npm install
+```
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+//milware
+app.use(express.static(__dirname + '/public'));
+
+//experss HBS engine 
+hbs.registerPartials(__dirname + '/views/parciales', function(err) {});
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    //res.send('Hello World')
+    /*let salida = {
+        nombre: 'Juan',
+        edad: 36,
+        url: req.url
+    };*/
+
+    res.render('home', {
+        nombre: 'Juan'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+/*
+app.get('/data', function(req, res) {
+    //res.send('Hello World')
+
+    res.send('Hola data');
+})
+*/
+
+//app.listen(3000)
+app.listen(3000, () => {
+    console.log('Escuchando peticiones en el puerto 3000')
+});
